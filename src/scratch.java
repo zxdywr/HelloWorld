3542aaaf80086a721b27628ea993777b37d25d06
zhangxin <zxdywr@qq.com>
2017-04-17 12:06:34 +0800

    updata12

--- src/scratch.java
+++ src/scratch.java
@@ -1,6 +1,6 @@
-public class c{
-    public static void main (String args[]){
-        String[] arg  =new String []{"hushfkushk","huiuhiuhiu"};
+public class c {
+    public static void main(String args[]) {
+        String[] arg = new String[]{"hushfkushk", "huiuhiuhiu"};
         System.out.println("arg = [" + arg + "]");
     }
 }
